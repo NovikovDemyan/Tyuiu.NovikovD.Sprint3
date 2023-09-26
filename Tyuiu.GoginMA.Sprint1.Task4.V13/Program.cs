cafa35520be4b37cf9d92887f551205a14a9e897
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-09-26 19:50:58 +0500

    Task4

---
+++ Tyuiu.GoginMA.Sprint1.Task4.V13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoginMA.Sprint1.Task4.V13.Lib;
+
+namespace Tyuiu.GoginMA.Sprint1.Task4.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Class Math                                                       *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #13                                                            *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,*");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.               *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            int x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("cos(PI / x) / (3 * E ^ (x + y)) = " + ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
