677c2e199c9088098c0b4aa49dcdfa5ff16bab05
Tyuiu.GoginMA@outlook.com <partycat53241@gmail.com>
2023-09-28 21:03:19 +0500

    Task6

---
+++ Tyuiu.GoginMA.Sprint1.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GoginMA.Sprint1.Task6.V6.Lib
+{
+    public class DataService : ISprint1Task6V6
+    {
+        public string DeleteFirstLetter(string value)
+        {
+            string[] words = value.Split(' ');
+            string Text = "";
+
+            foreach (string word in words)
+            {
+                if (word.Length > 1)
+                {
+                    string x = word.Substring(1);
+
+                    Text += x + " ";
+                }
+            }
+            return Text.Trim();
+        }
+    }
+}
---
+++ Tyuiu.GoginMA.Sprint1.Task6.V6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoginMA.Sprint1.Task6.V6.Lib;
+
+namespace Tyuiu.GoginMA.Sprint1.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Гогин М. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                  *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #6                                                             *");
+            Console.WriteLine("* Выполнил: Гогин Михаил Александрович | АСОиУБ-23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст.                         *");
+            Console.WriteLine("* Напечатать все слова, удалив из них первую букву.                      *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите строку:");
+            string value = Console.ReadLine();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine(ds.DeleteFirstLetter(value));
+            Console.ReadKey();
+        }
+    }
+}
